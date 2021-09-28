9cf784104d22267649c745be67df24ef303c7f01
kavitaparsewar <parsewarkavita@gmail.com>
2021-09-29 00:34:03 +0530

    [Kavita] UC11 emp wage using interface added

---
+++ UCLast_FinalSolution/UC11_Interface_ComputeEmpWage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_4_Assignment.UCLast_FinalSolution
+{
+    public interface UC11_Interface_ComputeEmpWage
+    {
+        void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMnth);
+        void computeEmpWage();
+
+        int getTotalWage(string company);
+    }
+}
